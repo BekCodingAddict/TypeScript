b57ae7eb382f889c6bbc0ccb47c8d699d24d227b
unknown <experte001@gmail.com>
2024-10-30 01:13:55 +0900

    Data Types

--- Fundamentals/TypeScript Tutorial for Beginners/Hello-World/src/index.ts
+++ Fundamentals/TypeScript Tutorial for Beginners/Hello-World/src/index.ts
@@ -1,6 +1,17 @@
 // console.log("Hello World");
 
-let age: number = 20;
-if (age < 10) {
-  age += 50;
+// let age: number = 20;
+// if (age < 10) {
+//   age += 50;
+// }
+
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level; // any
+
+function render(document: any) {
+  console.log(document);
 }
+
+render(level);
