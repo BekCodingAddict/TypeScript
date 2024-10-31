505f7a07b3d0cbee1b1de63e76b9342c6c3cb89f
unknown <experte001@gmail.com>
2024-10-31 13:05:44 +0900

    TypeScript union,intersection,literal,nullable tuypes and optional chainings

--- Fundamentals/TypeScript Tutorial for Beginners/Hello-World/dist/index.js
+++ Fundamentals/TypeScript Tutorial for Beginners/Hello-World/dist/index.js
@@ -1,7 +1,35 @@
 "use strict";
-const small = 1;
-const medium = 2;
-const large = 3;
-let mySize = 2;
-console.log(mySize);
+var _a;
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weigth) {
+    if (typeof weigth === "number")
+        return weigth * 2.2;
+    else
+        return parseInt(weigth) * 2.2;
+}
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { brithday: new Date() };
+}
+let customer = getCustomer(12);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.brithday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = (message) => console.log(message);
+log === null || log === void 0 ? void 0 : log("a");
 //# sourceMappingURL=index.js.map
--- Fundamentals/TypeScript Tutorial for Beginners/Hello-World/src/index.ts
+++ Fundamentals/TypeScript Tutorial for Beginners/Hello-World/src/index.ts
@@ -54,3 +54,71 @@
 
 // let date: Date = new Date();
 // console.log(employee.retire(date));
+
+// Advanced types
+//type alies
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+let employee: {} = {
+  id: 1,
+  name: "Mosh",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// union types
+function kgToLbs(weigth: number | string): number {
+  //Narrowing
+  if (typeof weigth === "number") return weigth * 2.2;
+  else return parseInt(weigth) * 2.2;
+}
+
+// intersection types
+// let weigth: number & string;
+
+type Draggable = {
+  drag: () => void;
+};
+
+type Resize = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resize;
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+//literal types
+type QUantity = 50 | 100;
+let quantity: QUantity = 100;
+
+type Metric = "cm" | "inch";
+
+//nullable types
+function greet(name: string | null | undefined) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Hola!");
+}
+greet(null);
+
+// optrinal chanining
+type Customer = {
+  brithday: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { brithday: new Date() };
+}
+
+let customer = getCustomer(12);
+console.log(customer?.brithday?.getFullYear());
+// customer?.[0]
+
+let log: any = (message: string) => console.log(message);
+log?.("a");
